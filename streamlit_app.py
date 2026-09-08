754110b079b69af56bd495c2f5ad5e2671fdb48b
uspriyan <uspriyan2008@gmail.com>
2026-09-08 14:10:01 +0530

    Add expander for penguin species table
    Added an expander for the penguin table in the Streamlit app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,6 @@ import streamlit as st
 import pandas as pd
 st.title('Machine Learning Model')
 st.info('here we are going to find Penguin Species')
-df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
-df
+with st.expander('Penguine Table'):
+  df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
+  df
