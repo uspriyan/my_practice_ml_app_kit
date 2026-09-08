fac5da2e6ad12a4601c605636c964500d45b2980
uspriyan <uspriyan2008@gmail.com>
2026-09-08 15:05:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,10 @@ with st.expander('Penguine Table'):
   df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
   df
   st.write('X-value')
-  x=df.drop('species',axis=2)
+  x=df.drop('species',axis=1)
   x
   st.write('Y-value')
-  y=df.species
+  y=df[['bill_depth_mm','flipper_length_mm','sex']]
   y
   
   
