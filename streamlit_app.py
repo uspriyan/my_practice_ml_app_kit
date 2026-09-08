7e2f00c3be92e034a5adffb57ea31f65e8a553c0
uspriyan <uspriyan2008@gmail.com>
2026-09-08 14:44:55 +0530

    Add info message for penguin data visualization
    Added an info message to the data visualization section.

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ with st.expander('Penguine Table'):
   df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
   df
 with st.expander('Data Visualization'):
+  st.info('Graph between Length of Penguin VS Body Mass of Penguine')
   #"island","bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g","sex
   st.scatter_chart(data=df,x='bill_length_mm',y='body_mass_g',color='species')
   
