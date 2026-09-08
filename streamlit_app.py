ecfcd67834451fc07f72e33c35be37f6b823f559
uspriyan <uspriyan2008@gmail.com>
2026-09-08 14:42:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,7 @@ st.info('here we are going to find Penguin Species')
 with st.expander('Penguine Table'):
   df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
   df
+with st.expander('Data Visualization'):
+  #"island","bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g","sex
+  st.scatter_plot(data=df,x='bill_length_mm',y='body_mass_g',color='species')
+  
