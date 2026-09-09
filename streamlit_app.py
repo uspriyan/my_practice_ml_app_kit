33e5bcdfe330a7ee4fd37cc47a46de9ba2eb47eb
uspriyan <uspriyan2008@gmail.com>
2026-09-09 20:42:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,17 +2,17 @@ import streamlit as st
 import pandas as pd
 st.title('Machine Learning Model')
 st.info('here we are going to find Penguin Species')
-with st.expander('Penguine Table'):
   df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
+ x=df.drop('species',axis=1)
+ y_raw=df[['species']]
+dummy_df1=pd.get_dummies(df)
+with st.expander('Penguine Table'):
   df
   st.write('X-value')
-  x=df.drop('species',axis=1)
   x
   st.write('Y-value')
-  y=df[['species']]
-  y
+  y_raw
   #encode
-  dummy_df1=pd.get_dummies(df,dtype= int )
   dummy_df1[:1]
 with st.expander('Data Visualization'):
   st.info('Graph between Length of Penguin VS Body Mass of Penguine')
